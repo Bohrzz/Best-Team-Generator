69c544fbb11cc603b20ddbed4567cba7d7a7f2c1
Bohrzz <113268086+Bohrzz@users.noreply.github.com>
2022-12-08 22:16:35 -0500

    fixed questions for the script.js

--- Assets/script.js
+++ Assets/script.js
@@ -3,54 +3,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const { generateHtml } = require("./utils/generateHtml");
 
-const employeeQuestions = [
-  {
-    name: "id",
-    type: "input",
-    message: "What is your employee id?",
-  },
-  {
-    name: "name",
-    type: "input",
-    message: "What is your name?",
-  },
-  {
-    name: "email",
-    type: "input",
-    message: "What is your e-mail?",
-  },
-
-  {
-    name: "position",
-    type: "list",
-    message: "What is your position?",
-    choices: ["manager", "intern", "engineer"],
-  },
-];
-
-const internQuestions = [
-  {
-    name: "school",
-    type: "input",
-    message: "What university did you go to or currently attendibg?",
-  },
-];
-
-const managerQuestions = [
-  {
-    name: "officenumber",
-    type: "input",
-    message: "What is your office number?",
-  },
-];
-const engineerQuestions = [
-  {
-    name: "username",
-    type: "input",
-    message: "What is your github username?",
-  },
-];
-
 async function init() {
   try {
     const answers = await inquirer.prompt(employeeQuestions).then((answers) => {
@@ -72,20 +24,163 @@ async function init() {
   } catch (err) {
     console.error(err);
   }
-  
- createManager(){
-  console.log("This is a new manager")
-  inquirer.prompt([{}]).then((newManager)=>{ const managerClass= new manager(
 
-    newManager.id,
-    newManager.name,
-    newManger.email,
-    newManager.position,
-    newManager.officenumber,
+  function createManager() {
+    console.log("This is a new manager");
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is your employee id?",
+        },
+        {
+          name: "name",
+          type: "input",
+          message: "What is your name?",
+        },
 
-  )})
-  
-  
- }
+        {
+          name: "email",
+          type: "input",
+          message: "What is your e-mail?",
+        },
+        {
+          name: "position",
+          type: "list",
+          message: "What is your position?",
+          choices: ["manager", "intern", "engineer"],
+        },
 
+        {
+          name: "officenumber",
+          type: "input",
+          message: "What is your office number?",
+        },
+      ])
+      .then((newManager) => {
+        const managerClass = new manager(
+          newManager.id,
+          newManager.name,
+          newManager.email,
+          newManager.position,
+          newManager.officenumber
+        );
+      });
+  }
+
+  function createIntern() {
+    console.log("This is a new intern");
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is your employee id?",
+        },
+        {
+          name: "name",
+          type: "input",
+          message: "What is your name?",
+        },
+
+        {
+          name: "email",
+          type: "input",
+          message: "What is your e-mail?",
+        },
+        {
+          name: "position",
+          type: "list",
+          message: "What is your position?",
+          choices: ["manager", "intern", "engineer"],
+        },
+
+        {
+          name: "school",
+          type: "input",
+          message: "What university did you go to or currently attendibg?",
+        },
+      ])
+      .then((newIntern) => {
+        const internClass = new intern(
+          newIntern.id,
+          newIntern.name,
+          newIntern.email,
+          newIntern.position,
+          newIntern.school
+        );
+      });
+  }
+
+  function createEngineer() {
+    console.log("This is a new engineer");
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is your employee id?",
+        },
+        {
+          name: "name",
+          type: "input",
+          message: "What is your name?",
+        },
+
+        {
+          name: "email",
+          type: "input",
+          message: "What is your e-mail?",
+        },
+        {
+          name: "position",
+          type: "list",
+          message: "What is your position?",
+          choices: ["manager", "intern", "engineer"],
+        },
+
+        {
+          name: "username",
+          type: "input",
+          message: "What is your github username?",
+        },
+      ])
+      .then((newEngineer) => {
+        const engineerClass = new engineer(
+          newEngineer.id,
+          newEngineer.name,
+          newEngineer.email,
+          newEngineer.position,
+          newEngineer.username
+        );
+      });
+  }
+
+  function createTeam() {
+    inquirer.prompt(
+      {
+        name: "add a team member",
+        type: "list",
+        message: "What team member do you want to add?",
+        choices: ["manager", "intern", "engineer", "none"],
+      },
+
+      then((newTeamMember) => {
+        switch (("intern", "engineer", "none")) {
+          case "intern":
+            createIntern()
+            break;
+          case "engineer":
+            createEngineer()
+            break
+
+          default:
+            break;
+
+        }
+      })
+    );
+  }
 }
+
